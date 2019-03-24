dcac711b42583dd69b9a2e4b48cd1292503e9bc1
Braden Goodwin <goodwin.braden@gmail.com>
2019-03-24 17:01:31 -0400

    Search displays images properly

--- vuescript.js
+++ vuescript.js
@@ -1,9 +1,11 @@
+// For making searchData global so it can be accessed on the results page
 let store = {
   searchData: {
     searchTerm: ''
   }
 }
 
+// Landing page for accepting search queries
 var bigSearch = new Vue({
   el: '#bar-button-container',
   data: {
@@ -28,15 +30,27 @@ var bigSearch = new Vue({
   }
 })
 
+// For displaying the search results on the results.html page
 var resultsApp = new Vue({
   el: '#results-app',
   data: {
     searchData: JSON.parse(window.localStorage.getItem("localStore")).searchData, //get same searchData from previous page
-    searchResults: null
+    searchResults: ''
   },
   mounted () {
     axios
       .get('https://images-api.nasa.gov/search?q=' + this.searchData.searchTerm)
-      .then(response => (this.searchResults = response))
+      .then(response => (this.searchResults = response.data.collection.items))
+  },
+  methods: {
+    displayMetadata: function() {
+
+    }
   }
+})
+
+// New vue component for displaying images
+Vue.component('image-results', {
+  props: ['search_item'],
+  template: '<img :src="search_item.links[0].href" style="height: 200px;"/>'
 })
